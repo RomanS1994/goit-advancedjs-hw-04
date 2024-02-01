b853d7b0afd0f4dbf7dc4684314550c074bcf2d6
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-02-01 23:15:10 +0100

    add url and apy_kay

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.search-form');
+form.addEventListener('submit', handlerSubmit);
 
+function handlerSubmit(evt) {
+  evt.preventDefault();
+  const { value } = evt.target.elements.searchQuery;
+  console.log(value);
+}
+
+//---------------------------------------------
+/************ HTTP-запити ************/
+
+const APY_KEY = '42159732-a54f57e537715c3d1f59422b1';
+const BASE_URL = 'https://pixabay.com/api/';
